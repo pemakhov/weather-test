0d770e74ce53b19d5c3a01ed865b770f59a679e6
pemakhov <serhiy.pemakhov@gmail.com>
2020-02-07 10:56:54 +0200

    style: make project eslint eligible
    Changed code to be eslint eligible. Personal API key replaced into apiconfig.js file (the key is
    absent there and should be put by a user).

---
+++ apiconfig.js
@@ -0,0 +1,4 @@
+
+const API_KEY = '';
+
+module.exports = API_KEY;
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,26 @@
-var express = require('express');
-var router = express.Router();
-var fetch = require('node-fetch');
-var forecast = require('../app/forecast');
+
+const express = require('express');
+const fetch = require('node-fetch');
+const API_KEY = require('../apiconfig');
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', (req, res) => {
   res.render('index');
 });
 
 /* Get forecast data. */
-router.post('/', function (req, res) {
-  fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${req.body.city}&APPID=`)
-    .then(res => res.json())
-    .then(json => res.send(json));
+router.post('/', (req, res) => {
+  fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${req.body.city}&APPID=${API_KEY}`)
+    .then((data) => data.json())
+    .then((json) => res.send(json));
 });
 
 /* POST weather table. */
-router.post('/data', function (req, res) {
-	const data = JSON.parse(req.body.cityData);
-  res.render('weather-table', {cityData: data});
+router.post('/data', (req, res) => {
+  const data = JSON.parse(req.body.cityData);
+  res.render('weather-table', { cityData: data });
 });
 
 module.exports = router;
-
